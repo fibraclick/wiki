15785a421fc420ac529e51426b59d9eb5a9549e9
Francesco Tonini <francesco@tonini.dev>
2019-08-23 14:52:12 +0200

    Remove deploy-cache

--- .semaphore/deploy.sh
+++ .semaphore/deploy.sh
@@ -2,17 +2,6 @@
 set -e
 cd $DESTINATION
 
-if [ -d "releases/$REVISION" ];
-then
-    echo "Removing: releases/$REVISION"
-    rm -rf releases/$REVISION;
-fi
-if [ ! -d "releases/$REVISION" ];
-then
-    echo "Creating: releases/$REVISION"
-    cp -dR deploy-cache releases/$REVISION;
-fi
-
 echo "Linking current to revision: $REVISION"
 rm -f current
 ln -s releases/$REVISION current
