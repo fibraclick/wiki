3666202fae85894e160fa5d3bdeda1ecb09b282c
Matteo Contrini <matteo@contrini.it>
2024-05-26 13:08:40 +0200

    Update newsletter archive function

--- functions/newsletter/index.json.js
+++ functions/newsletter/index.json.js
@@ -1,5 +1,42 @@
 export async function onRequest(context) {
-    const url = new URL('https://us20.api.mailchimp.com/3.0/campaigns');
+    // Get listmonk campaigns
+    let url = new URL('https://newsletter.fibra.click/api/campaigns');
+    url.searchParams.set('no_body', 'true');
+    url.searchParams.set('status', 'finished');
+    url.searchParams.set('per_page', 'all');
+    url.searchParams.set('order_by', 'created_at');
+    url.searchParams.set('order', 'DESC');
+
+    let response = await fetch(url, {
+        headers: {
+            Authorization: 'Basic ' + btoa(context.env.LISTMONK_CREDENTIALS)
+        }
+    });
+
+    if (!response.ok) {
+        console.log('Error fetching newsletter data (lm)');
+        console.log('Status: ' + response.status);
+        console.log('Body: ' + await response.text());
+        return new Response('', {
+            status: 500,
+            statusText: 'Error fetching newsletter data (lm)'
+        });
+    }
+
+    let data = await response.json();
+
+    let campaigns = data['data']['results']
+        .map(campaign => {
+            return {
+                url: `https://newsletter.fibra.click/archive/${campaign.archive_slug}`,
+                title: campaign.subject,
+                date: campaign.archive_slug
+            };
+        })
+        .filter(c => c.date.match(/^\d{4}-\d{2}-\d{2}$/));
+
+    // Get mailchimp campaigns
+    url = new URL('https://us20.api.mailchimp.com/3.0/campaigns');
     url.searchParams.set('type', 'regular');
     url.searchParams.set('status', 'sent');
     url.searchParams.set('fields', 'campaigns.long_archive_url,campaigns.settings.subject_line,campaigns.settings.title');
@@ -7,25 +44,25 @@ export async function onRequest(context) {
     url.searchParams.set('sort_field', 'send_time');
     url.searchParams.set('sort_dir', 'DESC');
 
-    let response = await fetch(url, {
+    response = await fetch(url, {
         headers: {
             Authorization: 'Bearer ' + context.env.MAILCHIMP_API_KEY
         }
     });
 
     if (!response.ok) {
-        console.log('Error fetching newsletter data');
+        console.log('Error fetching newsletter data (mc)');
         console.log('Status: ' + response.status);
         console.log('Body: ' + await response.text());
         return new Response('', {
             status: 500,
-            statusText: 'Error fetching newsletter data'
+            statusText: 'Error fetching newsletter data (mc)'
         });
     }
 
-    let data = await response.json();
+    data = await response.json();
 
-    let campaigns = data.campaigns.map(campaign => {
+    let mcCampaigns = data['campaigns'].map(campaign => {
         return {
             url: campaign.long_archive_url,
             title: campaign.settings.subject_line.replace(/^#[0-9]+[:\.] /, ''),
@@ -33,13 +70,15 @@ export async function onRequest(context) {
         };
     });
 
-    let v2duplicates = campaigns.filter(campaign => campaign.date.length > 10);
-    campaigns = campaigns.filter(campaign => {
+    let v2duplicates = mcCampaigns.filter(campaign => campaign.date.length > 10);
+    mcCampaigns = mcCampaigns.filter(campaign => {
         if (campaign.date.length > 10) return false;
         let v2 = v2duplicates.find(v2 => v2.date.startsWith(campaign.date));
         if (v2) campaign.url = v2.url;
         return true;
     });
+
+    campaigns = campaigns.concat(mcCampaigns);
 
     return new Response(JSON.stringify(campaigns), {
         headers: {
--- functions/newsletter/index.xml.js
+++ functions/newsletter/index.xml.js
@@ -1,5 +1,43 @@
 export async function onRequest(context) {
-    const url = new URL('https://us20.api.mailchimp.com/3.0/campaigns');
+    // Get listmonk campaigns
+    let url = new URL('https://newsletter.fibra.click/api/campaigns');
+    url.searchParams.set('no_body', 'true');
+    url.searchParams.set('status', 'finished');
+    url.searchParams.set('per_page', 'all');
+    url.searchParams.set('order_by', 'created_at');
+    url.searchParams.set('order', 'DESC');
+
+    let response = await fetch(url, {
+        headers: {
+            Authorization: 'Basic ' + btoa(context.env.LISTMONK_CREDENTIALS)
+        }
+    });
+
+    if (!response.ok) {
+        console.log('Error fetching newsletter data (lm)');
+        console.log('Status: ' + response.status);
+        console.log('Body: ' + await response.text());
+        return new Response('', {
+            status: 500,
+            statusText: 'Error fetching newsletter data (lm)'
+        });
+    }
+
+    let data = await response.json();
+
+    let campaigns = data['data']['results']
+        .filter(c => c.archive_slug.match(/^\d{4}-\d{2}-\d{2}$/))
+        .map(campaign => {
+            return {
+                url: `https://newsletter.fibra.click/archive/${campaign.archive_slug}`,
+                title: campaign.subject,
+                date: campaign.send_at,
+                guid: campaign.archive_slug
+            };
+        });
+
+    // Get mailchimp campaigns
+    url = new URL('https://us20.api.mailchimp.com/3.0/campaigns');
     url.searchParams.set('type', 'regular');
     url.searchParams.set('status', 'sent');
     url.searchParams.set('fields', 'campaigns.long_archive_url,campaigns.settings.subject_line,campaigns.settings.title,campaigns.send_time');
@@ -7,25 +45,25 @@ export async function onRequest(context) {
     url.searchParams.set('sort_field', 'send_time');
     url.searchParams.set('sort_dir', 'DESC');
 
-    let response = await fetch(url, {
+    response = await fetch(url, {
         headers: {
             Authorization: 'Bearer ' + context.env.MAILCHIMP_API_KEY
         }
     });
 
     if (!response.ok) {
-        console.log('Error fetching newsletter data');
+        console.log('Error fetching newsletter data (mc)');
         console.log('Status: ' + response.status);
         console.log('Body: ' + await response.text());
         return new Response('', {
             status: 500,
-            statusText: 'Error fetching newsletter data'
+            statusText: 'Error fetching newsletter data (mc)'
         });
     }
 
-    let data = await response.json();
+    data = await response.json();
 
-    let campaigns = data.campaigns.map(campaign => {
+    let mcCampaigns = data['campaigns'].map(campaign => {
         return {
             url: campaign.long_archive_url,
             title: campaign.settings.subject_line,
@@ -34,13 +72,15 @@ export async function onRequest(context) {
         };
     });
 
-    let v2duplicates = campaigns.filter(campaign => campaign.guid.length > 10);
-    campaigns = campaigns.filter(campaign => {
+    let v2duplicates = mcCampaigns.filter(campaign => campaign.guid.length > 10);
+    mcCampaigns = mcCampaigns.filter(campaign => {
         if (campaign.guid.length > 10) return false;
         let v2 = v2duplicates.find(v2 => v2.guid.startsWith(campaign.guid));
         if (v2) campaign.url = v2.url;
         return true;
     });
+
+    campaigns = campaigns.concat(mcCampaigns);
 
     let xml = `<?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
